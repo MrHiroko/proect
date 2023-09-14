ebedd2a7b2ba71ab4f4cbe9c89b74fe6f165d3cf
Nikolay Pomazkin <koliapomazkin@yandex.ru>
2023-09-14 19:41:30 +0700

    new file:   dist/css/style.css 	new file:   dist/css/style.min.css 	new file:   dist/img/foto/1.webp 	new file:   dist/img/foto/2.webp 	new file:   dist/img/foto/3.webp 	new file:   dist/img/foto/bg.jpg 	new file:   dist/img/foto/bg.webp 	new file:   dist/img/foto/tractor.jpg 	new file:   dist/img/foto/tractor.webp 	new file:   dist/img/partners/ogk1.webp 	new file:   dist/img/partners/ogk_logo.webp 	new file:   dist/img/partners/unnamed.webp 	new file:   dist/index.html 	new file:   dist/js/app.min.js

---
+++ gulp/tasks/server.js
@@ -0,0 +1,17 @@
+
+export const server = (done) => {
+	app.plugins.browsersync.init({
+		server: {
+			baseDir: `${app.path.build.html}`
+		},
+		notify: false,
+		port: 3000,
+	});
+}
+
+
+
+
+
+
+
